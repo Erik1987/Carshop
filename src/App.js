d6b94993199aa580f650dc77651a550e82004d41
Erik <you@example.com>
2020-05-05 14:13:05 +0300

    eka ajo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import Carlist from './componets/Carlist'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+
+  return (
+    <div className="App">
+
+<AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+           Carlist
+          </Typography>
+     
+        </Toolbar>
+      </AppBar>
+      <Carlist />
+    </div>
+  );
+}
+
+export default App;
